51aa2b8f7161a0aedc1cad1432a4ac9df66a54d8
bruce.lxm <bruce.lxm@alibaba-inc.com>
2018-06-28 19:19:19 +0800

    dd.getAuthCode

--- page/index/index.js
+++ page/index/index.js
@@ -2,9 +2,11 @@ let app = getApp();
 //替换成开发者后台设置的安全域名
 let url = "http://127.0.0.1:8080";
 
-//内网穿透工具介绍:
-// https://open-doc.dingtalk.com/microapp/debug/ucof2g
-//let url = "http://your_sub_domain.vaiwan.com/";
+//let url = "http://abcde.vaiwan.com";
+//若要在测试应用中临时使用类似abcdef.vaiwan.com 的二级域名代理到无公网IP的服务端开发环境，
+//请参考内网穿透工具介绍:
+//https://open-doc.dingtalk.com/microapp/debug/ucof2g
+
 
 Page({
     data:{
@@ -20,12 +22,13 @@ Page({
             corpId: app.globalData.corpId
         })
 
-         
-        dd.getCorpAuthCode({
+
+        dd.getAuthCode({
             success:(res)=>{
                 _this.setData({
                     authCode:res.authCode
                 })
+            
                 
                 dd.httpRequest({
                     url: url+'/login',
